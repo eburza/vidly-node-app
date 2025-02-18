19f891346595512d5f51d1aa6f57c382e22d08c6
Emilia Burza <emila.burza@gmail.com>
2025-02-18 12:11:52 +0100

    Update movie schema

--- models/genre.js
+++ models/genre.js
@@ -1,22 +1,27 @@
 const Joi = require('joi');
 const mongoose = require('mongoose');
 
-const Genre = mongoose.model('Genre', new mongoose.Schema({
+//mongoose schema
+const genreSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
     minlength: 5,
     maxlength: 50
   }
-}));
+})
 
+const Genre = mongoose.model('Genre', genreSchema);
+
+//Joi schema for validation
 function validateGenre(genre) {
   const schema = {
-    name: Joi.string().min(3).required()
+    name: Joi.string().min(5).max(50).required()
   };
 
   return Joi.validate(genre, schema);
 }
 
+exports.genreSchema = genreSchema; 
 exports.Genre = Genre; 
 exports.validate = validateGenre;
