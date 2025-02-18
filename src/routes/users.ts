9461b775efdbe5619df6adfbc3751a59822d1a21
Emilia Burza <emila.burza@gmail.com>
2025-02-18 19:24:33 +0100

    Add user password hashing library

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,5 +1,6 @@
-const lodash = require('lodash');
-const { passwordStrength } = require('check-password-strength')
+const lodash = require('lodash'); // object manipulation
+//const { passwordStrength } = require('check-password-strength') // password strength check
+const bcrypt = require('bcrypt'); // password hashing
 const express = require('express');
 const router = express.Router();
 const { User, validate } = require('../models/user');
@@ -16,19 +17,24 @@ router.post('/', async (req: Request, res: Response) => {
   if (error) return res.status(400).send(error);
 
   //Check if the password is strong
-  const passwordCheck = passwordStrength(req.body.password);
+  // const passwordCheck = passwordStrength(req.body.password);
 
-  if (passwordCheck < 2) { // 0 = Too weak, 1 = Weak, 2 = Medium, 3 = Strong
-    return res.status(400).send({
-      error: 'Password is too weak. Password must contain uppercase, lowercase, numbers and special characters.'
-    });
-  }
+  // if (passwordCheck < 2) { // 0 = Too weak, 1 = Weak, 2 = Medium, 3 = Strong
+  //   return res.status(400).send({
+  //     error: 'Password is too weak. Password must contain uppercase, lowercase, numbers and special characters.'
+  //   });
+  // }
 
   //check if the user already exists
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send('User already registered.');
 
   user = new User(lodash.pick(req.body, ['name', 'email', 'password']));
+  
+  //password hashing
+  const salt = await bcrypt.genSalt(10); // generate a salt to make the password more secure
+  const hashedPassword = await bcrypt.hash(user.body.password, salt); // hash the password
+  user.password = hashedPassword; // replace the password with the hashed password
 
   await user.save();
 
