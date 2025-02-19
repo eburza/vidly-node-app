e06eb39b791156121f5a7d6986a5367e6ab87e3c
Emilia Burza <emila.burza@gmail.com>
2025-02-19 14:25:45 +0100

    Add cofig file to index.ts
    Check if JSON web token is set

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
 const mongoose = require('mongoose');
+const config = require('config') // config file
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const rentals = require('./routes/rentals');
 const users = require('./routes/users');
+const auth = require('./routes/auth');
 const express = require('express');
 const app = express();
 require('dotenv').config({ path: '.env.local' });
+
+//check if the jwtPrivateKey is set
+if (!config.get('jwtPrivateKey')) {
+  console.log('FATAL ERROR: jwtPrivateKey is not defined.');
+  // throw new Error('FATAL ERROR: jwtPrivateKey is not defined.');
+  //exit the process
+  process.exit(1); //exit the process with a failure code, 0 is success
+}
 
 mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log('Connected to MongoDB'))
@@ -19,6 +29,7 @@ app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
